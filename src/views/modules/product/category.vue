7df0b78abea919744ebe26211e6a002a2881b4a4
Hugh <302884999@qq.com>
2022-03-16 22:00:48 +0800

    项目初始化

---
+++ src/views/modules/product/category.vue
@@ -0,0 +1,39 @@
+<!--  -->
+<template>
+  <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        data: [],
+        defaultProps: {
+          children: 'children',
+          label: 'label'
+        }
+      };
+    },
+    methods: {
+      handleNodeClick(data) {
+        console.log(data);
+      },
+      getMenus() {
+          this.$http({
+          url: this.$http.adornUrl('/product/category/list/tree'),
+          method: 'get',
+        }).then(({data}) => {
+          console.log("成功获取菜单数据", data)
+        })
+      }
+    },
+    created() {
+     this.getMenus();   
+    }
+  }
+  
+</script>
+
+</script>
+<style lang='scss' scoped>
+</style>
